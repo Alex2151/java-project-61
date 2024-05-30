1ea5e9ac992e6d8f143a76f141bb8bd1aeb070a5
Alex Spiridonow <spiridonow.sania@yandex.ru>
2024-05-30 15:42:06 +0300

    Modified build.gradle.kts file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,12 @@
 plugins {
-    id("java")
+    //java
+    id ("io.spring.dependency-management") version "1.1.5" //"1.0.11.RELEASE"
+    application
+    //id ("java-
+}
+
+application {
+    mainClass="hexlet.code.App"
 }
 
 group = "hexlet.code"
@@ -9,11 +16,51 @@ repositories {
     mavenCentral()
 }
 
+dependencyManagement {
+    imports {
+	mavenBom ("org.springframework.boot:spring-boot-dependencies:3.1.1")  //2.5.4")
+    }
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    //implementation("org.springframework.integration:spring-integration-core")
+    //
 }
+
 
 tasks.test {
     useJUnitPlatform()
+}
+
+//Groovy
+/*
+tasks managedVersions {
+    doLast {
+	dependencyManagement.managedVersions.each {
+	    println it
+	}
+    }
+}
+*/
+
+task("managedVersions") {
+    group = "Custom"
+    description = "Prints all compatible versions of libraries"
+
+    doLast {
+        dependencyManagement.managedVersions.forEach {
+            println(it)
+        }
+    }
+}
+
+tasks.register("hello") {
+    group = "Custom"
+    description = "A lovely greeting task."
+
+    doLast {
+	println("Hello, world!")
+    }
 }
