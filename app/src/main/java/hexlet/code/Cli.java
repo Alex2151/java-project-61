76138df8b666496a709e40d00f8d0823285f9672
Alex Spiridonow <spiridonow.sania@yandex.ru>
2024-05-31 15:52:05 +0300

    Checkstyle added

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 public class Cli {
     public static void sayHello() {
-	System.out.print("May I have your name? ");
+        System.out.print("May I have your name? ");
 
-	Scanner console = new Scanner(System.in);
-	/*if (console.hasNext()) {
-	    String name = console.next();
-	    System.out.println("Has read name!");
-	} else {
-	    System.out.println("Got nothing to read!");
-	}*/
-	String name = console.next();
+        Scanner console = new Scanner(System.in);
+        /*if (console.hasNext()) {
+            String name = console.next();
+            System.out.println("Has read name!");
+        } else {
+            System.out.println("Got nothing to read!");
+        }*/
+        String name = console.next();
 
-	System.out.println("Hello, " + name + "!");
+        System.out.println("Hello, " + name + "!");
 
-	console.close();
-     }
+        console.close();
+    }
 }
